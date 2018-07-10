4a77b955ef80ff215c88d2e63d043b74054da62c
qbicfeet <qbicfeet@swords.dance>
2018-07-10 20:21:50 +0200

    Create PlaylistTokenType enum

---
+++ src/Hls/PlaylistTokenType.cs
@@ -0,0 +1,36 @@
+﻿namespace SwordsDance.Hls
+{
+    /// <summary>Specifies HLS playlist token types.</summary>
+    public enum PlaylistTokenType
+    {
+        /// <summary>
+        /// Returned by <see cref="PlaylistTextReader.TokenType"/> if data from the underlying playlist has yet to
+        /// be read.
+        /// </summary>
+        None,
+
+        /// <summary>A URI.</summary>
+        Uri,
+
+        /// <summary>A blank line.</summary>
+        Blank,
+
+        /// <summary>A comment.</summary>
+        Comment,
+
+        /// <summary>A tag name.</summary>
+        TagName,
+
+        /// <summary>A tag value.</summary>
+        TagValue,
+
+        /// <summary>An attribute name.</summary>
+        AttributeName,
+
+        /// <summary>A quoted attribute value.</summary>
+        QuotedAttributeValue,
+
+        /// <summary>An unquoted attribute value.</summary>
+        UnquotedAttributeValue,
+    }
+}
