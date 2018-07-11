939b5bcf8ab3f939f76bbeea66622a733904d094
qbicfeet <qbicfeet@swords.dance>
2018-07-11 13:53:15 +0200

    Update PlaylistToken class

--- src/Hls/PlaylistToken.cs
+++ src/Hls/PlaylistToken.cs
@@ -8,11 +8,26 @@
         /// </summary>
         /// <param name="type">The type of the token.</param>
         /// <param name="value">The value of the token.</param>
-        public PlaylistToken(PlaylistTokenType type, string value)
+        public PlaylistToken(PlaylistTokenType type, string value, int line, int column)
         {
             Type = type;
             Value = value;
+            Line = line;
+            Column = column;
         }
+
+        /// <summary>Gets the character position of the token.</summary>
+        /// <remarks>
+        /// The returned value reflects the 1-based index of the first character of the value of the token.
+        /// </remarks>
+        public int Column { get; }
+
+        /// <summary>Gets the line number of the token.</summary>
+        /// <remarks>
+        /// The returned value reflects the 1-based index of the line containing the first character of the value of
+        /// the token.
+        /// </remarks>
+        public int Line { get; }
 
         /// <summary>Gets the type of the token.</summary>
         public PlaylistTokenType Type { get; }
@@ -22,6 +37,6 @@
 
         /// <summary>Returns the string representation of the token.</summary>
         /// <returns>The string representation of the token.</returns>
-        public override string ToString() => "[" + Type + ": " + Value + "]";
+        public override string ToString() => "[" + Type + "] " + Value + " (" + Line + ", " + Column + ")";
     }
 }
