0dd56a1d18a21f7d2576b7e48d21001258d0cb35
qbicfeet <qbicfeet@swords.dance>
2018-07-14 22:51:44 +0200

    Add PlaylistTextReaderOptions enum

---
+++ src/Hls/PlaylistTextReaderOptions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace SwordsDance.Hls
+{
+    /// <summary>Specifies options for <see cref="PlaylistTextReader"/>.</summary>
+    [Flags]
+    public enum PlaylistTextReaderOptions
+    {
+        /// <summary>Specifies that all options should be disabled.</summary>
+        None = 0,
+
+        /// <summary>
+        /// Specifies that <see cref="PlaylistTextReader.Read"/> should emit all tokens it reads, including markers,
+        /// separators, terminators and end-of-line sequences.
+        /// </summary>
+        Verbose = 1 << 0,
+
+        /// <summary>The default set of options used by <see cref="PlaylistTextReader"/> instances.</summary>
+        Default = Verbose,
+    }
+}
